2ed182a4332eda91bb2692a76eee39bb0a2dc332
Tamal Kundu <tamalkundu2002@gmail.com>
2024-11-12 14:03:24 +0530

    commit

---
+++ frontend/src/components/Topbar.tsx
@@ -0,0 +1,46 @@
+import { SignedOut, UserButton } from "@clerk/clerk-react";
+import { LayoutDashboardIcon } from "lucide-react";
+import { Link } from "react-router-dom";
+import SignInOAuthButtons from "./SignInOAuthButtons";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "./ui/button";
+
+const Topbar = () => {
+  const { isAdmin } = useAuthStore();
+  console.log({ isAdmin });
+
+  return (
+    <div
+      className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 
+      backdrop-blur-md z-10
+    "
+    >
+      <div className="flex gap-2 items-center">
+        <img src="/spotify.png" className="w-8 h-8" alt="Spotify logo" />
+        <h1 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
+          Beatify
+        </h1>
+      </div>
+
+      <div className="flex items-center gap-4">
+        {isAdmin && (
+          <Link
+            to={"/admin"}
+            className={cn(buttonVariants({ variant: "outline" }))}
+          >
+            <LayoutDashboardIcon className="size-4  mr-2" />
+            Admin Dashboard
+          </Link>
+        )}
+
+        <SignedOut>
+          <SignInOAuthButtons />
+        </SignedOut>
+
+        <UserButton />
+      </div>
+    </div>
+  );
+};
+export default Topbar;
